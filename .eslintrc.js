bf79060af0e4ad07b580b6556325a1fda3d737f9
Pedro Araujo <pedro-foa@hotmail.com>
2019-10-19 17:08:32 -0300

    blal

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,23 @@
 module.exports = {
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-    'prettier'
-  ],
-  plugins: ['prettier'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  rules: {
-    "prettier/prettier": "error",
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    "camelcase": "off",
-    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
-  },
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 };
