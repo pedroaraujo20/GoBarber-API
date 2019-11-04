c7c7a4696268bba56263964ceac7e9931f7e9335
Pedro Araujo <pedro-foa@hotmail.com>
2019-11-04 13:23:02 -0300

    made some changes on session, schedule and edit user

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import express from 'express';
 import 'express-async-errors';
 import Youch from 'youch';
 import path from 'path';
+import cors from 'cors';
 import * as Sentry from '@sentry/node';
 import routes from './routes';
 import sentryConfig from './config/sentry';
@@ -23,6 +24,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
